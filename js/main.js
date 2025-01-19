ccc10df00b42bbd26a20c83d26b3f53cd72bd1de
fhermas22 <franciscohermas@gmail.com>
2025-01-19 15:52:13 +0100

    Modification et Ajustement du contenu de la section Services

--- js/main.js
+++ js/main.js
@@ -160,44 +160,9 @@ ScrollReveal().reveal('.experience-card, .service-card, .education, .portfolio .
 ScrollReveal().reveal('footer .group', {delay: 500, origin: 'top', interval: 200});
 
 // Async Callbell Code
-if (!window.callbellSettings) {
-  window.callbellSettings = {};
-}
-window.callbellSettings["token"] = "sY1qHzsUewH4gVuzG52QLaK5";
+// if (!window.callbellSettings) {
+//   window.callbellSettings = {};
+// }
+// window.callbellSettings["token"] = "sY1qHzsUewH4gVuzG52QLaK5";
 
-(function () {
-  var w = window;
-  var ic = w.callbell;
-  if (typeof ic === "function") {
-    ic("reattach_activator");
-    ic("update", callbellSettings);
-  } else {
-    var d = document;
-    class i {
-          constructor() {
-              i.c(arguments);
-          }
-          static c(args) {
-              i.q.push(args);
-          }
-      }
-    i.q = [];
-    w.Callbell = i;
-    var l = function () {
-      var s = d.createElement("script");
-      s.type = "text/javascript";
-      s.async = true;
-      s.src =
-        "https://dash.callbell.eu/include/" +
-        window.callbellSettings.token +
-        ".js";
-      var x = d.getElementsByTagName("script")[0];
-      x.parentNode.insertBefore(s, x);
-    };
-    if (w.attachEvent) {
-      w.attachEvent("onload", l);
-    } else {
-      w.addEventListener("load", l, false);
-    }
-  }
-})();
+// (function(){var w=window;var ic=w.callbell;if(typeof ic==="function"){ic('reattach_activator');ic('update',callbellSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Callbell=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://dash.callbell.eu/include/'+window.callbellSettings.token+'.js';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()
