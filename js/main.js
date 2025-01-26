7408f7162851ef825ab87869c5364fdd8679e753
fhermas22 <franciscohermas@gmail.com>
2025-01-26 16:46:23 +0100

    Divers ajustements concernant la responsivité, et quelques éléments débordants

--- js/main.js
+++ js/main.js
@@ -151,6 +151,7 @@ ScrollReveal({
 ScrollReveal().reveal('.home .info h2, .section-title-01, .section-title-02', {delay: 500, origin: 'left'});
 ScrollReveal().reveal('.home .info h3, .home .info p, .about-info .btn', {delay: 600, origin: 'right'});
 ScrollReveal().reveal('.home .info .btn', {delay: 700, origin: 'bottom'});
+ScrollReveal().reveal('.scroll-down', {delay: 800, origin: 'bottom'});
 ScrollReveal().reveal('.media-icons i, .contact-left li', {delay: 500, origin: 'left', interval: 200});
 ScrollReveal().reveal('.home-img, .about-img', {delay: 500, origin: 'bottom'});
 ScrollReveal().reveal('.about .description, .contact-right', {delay: 600, origin: 'right'});
